58a0dc4c60b117ba9e7e335fb720a29d50546b85
Jujutsualfayo <alphaspaam@gmail.com>
2024-12-02 21:40:35 +0000

    updated util3

---
+++ utils/security.py
@@ -0,0 +1,8 @@
+def hash_password(password):
+    print("Hashing password...")
+    return "hashed_password"
+
+def verify_password(password, hashed_password):
+    print("Verifying password...")
+    return True
+
