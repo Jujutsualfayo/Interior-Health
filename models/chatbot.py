ed73175927ce2c29b1b95f73b6deb521637a959d
Jujutsualfayo <alphaspaam@gmail.com>
2024-12-02 21:13:24 +0000

    updated Ai1

---
+++ models/chatbot.py
@@ -0,0 +1,7 @@
+class Chatbot:
+    @staticmethod
+    def get_response(query):
+        """Allows for processing chatbot queries."""
+        print(f"Received query: {query}")
+        return "This is a sample response from the chatbot."
+
