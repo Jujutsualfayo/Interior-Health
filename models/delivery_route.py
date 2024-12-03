2c19be537809419f664fe11cf525f7d78ef104de
Jujutsualfayo <alphaspaam@gmail.com>
2024-12-03 15:44:27 +0000

    updated routes

---
+++ models/delivery_route.py
@@ -0,0 +1,16 @@
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+class DeliveryRoute(Base):
+    __tablename__ = 'delivery_routes'
+
+    id = Column(Integer, primary_key=True)
+    start_point = Column(String(255), nullable=False)
+    end_point = Column(String(255), nullable=False)
+    distance = Column(Integer, nullable=False)
+
+    def __init__(self, start_point, end_point, distance):
+        self.start_point = start_point
+        self.end_point = end_point
+        self.distance = distance
+
