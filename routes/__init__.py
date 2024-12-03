7db06a47a98b80b24122952c2240ff9dbc3e3f46
Jujutsualfayo <alphaspaam@gmail.com>
2024-12-03 21:18:21 +0000

    updated init

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,21 +1,26 @@
-# routes/__init__.py
+from flask import Blueprint
 
-from .user_routes import user_routes
-from .product_routes import product_routes
-from .order_routes import order_routes
-from .payment_routes import payment_routes
-from .tracking_routes import tracking_routes
-from .chatbot_routes import chatbot_routes
-from .teleconsultation_routes import teleconsultation_routes
+from .auth import auth_bp
+from .delivery_routes import delivery_bp
+from .medication_routes import medications_bp
+from .notifications import notifications_bp
+from .users_routes import users_bp
+from .orders_routes import orders_bp
+from .payments_routes import payments_bp
+from .chatbot_routes import chatbot_bp
+from .teleconsultations_routes import teleconsultations_bp
 
-# Combine all route modules for easier app inclusion
-routes = [
-    user_routes,
-    product_routes,
-    order_routes,
-    payment_routes,
-    tracking_routes,
-    chatbot_routes,
-    teleconsultation_routes,
-]
+# Register all routes into a single blueprint
+api_bp = Blueprint('api', __name__)
+
+# Add all sub-blueprints
+api_bp.register_blueprint(auth_bp, url_prefix='/auth')
+api_bp.register_blueprint(delivery_bp, url_prefix='/delivery')
+api_bp.register_blueprint(medications_bp, url_prefix='/medications')
+api_bp.register_blueprint(notifications_bp, url_prefix='/notifications')
+api_bp.register_blueprint(users_bp, url_prefix='/users')
+api_bp.register_blueprint(orders_bp, url_prefix='/orders')
+api_bp.register_blueprint(payments_bp, url_prefix='/payments')
+api_bp.register_blueprint(chatbot_bp, url_prefix='/chatbot')
+api_bp.register_blueprint(teleconsultations_bp, url_prefix='/teleconsultations')
 
