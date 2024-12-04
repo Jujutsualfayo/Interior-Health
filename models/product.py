f1ec4fad1a4788a3f82206354c5c8fd5295dd187
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-04 22:25:57 +0300

    Create product.py

---
+++ models/product.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Float
+from .database import db
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(255), nullable=True)
+    price = Column(Float, nullable=False)
+    stock = Column(Integer, nullable=False)
+
+    def __init__(self, name, description, price, stock):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.stock = stock
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "stock": self.stock,
+        }
